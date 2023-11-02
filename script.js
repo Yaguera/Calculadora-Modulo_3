26a7f1c9e42b3a1dab97285778464a726905037f
Yago Gomes <yaguamineral@gmail.com>
2023-11-02 17:26:32 -0300

    adicionado metodos de add digito e atualuizar o display

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const display = document.getElementById('display')
-const buttons = document.querySelectorAll('button')
+const displayText = document.getElementById('display-text');
+const buttons = document.querySelectorAll('button');
 console.log(buttons)
 
 class Calculadora {
+    constructor(displayText) {
+        this.displayText = displayText;
+        this.currentDisplayText = "";
+    }
 
+    adicionarDigito(digito) {
+        this.currentDisplayText = digito
+        this.updateScreen()
+    }
+    updateScreen() {
+        this.displayText.innerText += this.currentDisplayText;
+    }
 }
+
+const calc = new Calculadora(displayText)
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", function (e) {
         const value = e.target.innerText;
         if (parseFloat(value) >= 0 || value == ",") {
-            console.log(value)
+            calc.adicionarDigito(value)
         } else {
             console.log("OP: " + value)
         }
