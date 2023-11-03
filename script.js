c0dd9716d80a62fa5c1529c32f2c4dd758ba015a
Yago Gomes <yaguamineral@gmail.com>
2023-11-02 21:21:58 -0300

    metodos addDigit e processOperation

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
+
 const displayText = document.getElementById('display-text');
+const expressionText = document.getElementById('expression');
 const buttons = document.querySelectorAll('button');
-console.log(buttons)
 
-class Calculadora {
-    constructor(displayText) {
-        this.displayText = displayText;
-        this.currentDisplayText = "";
-    }
-
-    adicionarDigito(digito) {
-        this.currentDisplayText = digito
-        this.updateScreen()
-    }
-    updateScreen() {
-        this.displayText.innerText += this.currentDisplayText;
-    }
-}
-
-const calc = new Calculadora(displayText)
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", function (e) {
         const value = e.target.innerText;
-        if (parseFloat(value) >= 0 || value == ",") {
-            calc.adicionarDigito(value)
+        if (+value >= 0 || value === ".") { //+ trasnforma o value de string para number
+            calc.addDigit(value)
         } else {
-            console.log("OP: " + value)
+            calc.processOperation(value)
         }
     })
 })
+
+
+class Calculadora {
+    constructor(displayText, expressionText) {
+        this.displayText = displayText;
+        this.expressionText = expressionText;
+        this.digitPressed = "";
+        this.operation = "";
+    }
+
+    addDigit(digit) {
+        if (digit === "." && this.displayText.innerText.includes(".")) {
+            return
+        }
+        this.digitPressed = digit
+        this.updateScreen()
+        console.log(displayText.innerText)
+        console.log(this.digitPressed)
+    }
+
+
+
+    processOperation(operation) {
+        if (operation == "=" || operation == "DEL" || operation == "C" || operation == "CE") {
+            return
+        } else {
+            let newExpression = this.displayText.innerText + " " + operation + " "
+            this.displayText.innerText = ""
+            console.log(newExpression)
+            this.operation = newExpression
+            this.expressionText.innerText += this.operation;
+        }
+    }
+
+    updateScreen() {
+
+        this.displayText.innerText += this.digitPressed;
+
+    }
+
+}
+
+const calc = new Calculadora(displayText, expressionText)
